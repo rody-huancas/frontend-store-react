72263004ec3e550d9f269859ade2ac4f9093217f
Rody Huancas <rodyhuancas.04@gmail.com>
2023-12-09 19:34:23 -0500

    Fix: load services

--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 
 // api
 import { getAllCategoryService, getAllService } from "api/api";
-import { CardService } from "components";
+import { CardService, Spinner } from "components";
 
 export const Service = () => {
   const [services, setServices] = useState([]);
@@ -28,37 +28,40 @@ export const Service = () => {
   }, []);
 
   return (
-    <div className="flex flex-col gap-5 text-gray-600 py-10">
-      <h1 className="text-center text-3xl font-extrabold uppercase">
-        Nuestros Servicios
-      </h1>
+    <>
+      {load ? (
+        <div className="w-full h-80 flex justify-center items-center">
+          <Spinner />
+        </div>
+      ) : (
+        <div className="flex flex-col gap-5 text-gray-600 py-10">
+          <h1 className="text-center text-3xl font-extrabold uppercase">
+            Nuestros Servicios
+          </h1>
 
-      <div className="flex flex-col gap-5">
-        {categoryServices.map((category) => (
-          <div key={category._id} className="flex flex-col gap-5">
-            <h2 className="font-bold uppercase mt-5">{category.name}</h2>
-            <div
-              className={`grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-${Math.min(
-                3,
-                services.filter(
-                  (service) => service.categoryService._id === category._id
-                ).length
-              )} place-items-center gap-5`}
-            >
-              {services
-                .filter(
-                  (service) => service.categoryService._id === category._id
-                )
-                .map((filteredService) => (
-                  <CardService
-                    key={filteredService._id}
-                    services={filteredService}
-                  />
-                ))}
-            </div>
+          <div className="flex flex-col gap-5">
+            {categoryServices.map((category) => (
+              <div key={category._id} className="flex flex-col gap-5">
+                <h2 className="font-bold uppercase mt-5">{category.name}</h2>
+                <div
+                  className={`grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 place-items-center place-content-center gap-5`}
+                >
+                  {services
+                    .filter(
+                      (service) => service.categoryService._id === category._id
+                    )
+                    .map((filteredService) => (
+                      <CardService
+                        key={filteredService._id}
+                        services={filteredService}
+                      />
+                    ))}
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-    </div>
+        </div>
+      )}
+    </>
   );
 };
